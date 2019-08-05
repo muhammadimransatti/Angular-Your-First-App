283c86f9e17687f042bb6561437db681dd8c79c0
muhammadimransatti <mimransatti5@gmail.com>
2019-08-04 20:59:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xb2wc8-phoykc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xb2wc8-phoykc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  
+  products: [] = [
+    {name: "Phone XL", description : "A large phone with one of the best screens"},
+    {name: "Phone Mini", description: "A great phone with one of the best cameras"},
+    {name: "Phone Standard"},
+    ];
+    share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
